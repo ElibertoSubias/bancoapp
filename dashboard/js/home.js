b89f531345b93810805cb03cb48fba95c15a8615
Eliberto Subias <eliberto.subias.ortega@gmail.com>
2022-07-28 18:52:09 -0600

    Se crea funcion de tranferencia

--- dashboard/js/home.js
+++ dashboard/js/home.js
@@ -14,8 +14,12 @@ const cargarMovimientos = (movimientos) => {
                 tipoMovimiento = " - Retiro por: ";
             } else if (element.tipo == 1) {
                 tipoMovimiento = " - Deposito por ";
+            } else if (element.tipo == 2) {
+                tipoMovimiento = " - Tranferencia por ";
+            } else if (element.tipo == 3) {
+                tipoMovimiento = " - Deposito de tranferencia por ";
             }
-            liElement.innerHTML = element.fecha ? element.fecha + tipoMovimiento : '00/00/0000' + tipoMovimiento;
+            liElement.innerHTML = element.fecha ? element.fecha + tipoMovimiento : '00-00-0000' + tipoMovimiento;
             const strongElement = document.createElement("strong");
             strongElement.innerHTML = parseFloat(element.cantidad).toLocaleString('en-US', {style: 'currency',currency: 'USD', minimumFractionDigits: 2});;
             liElement.appendChild(strongElement);
@@ -35,7 +39,7 @@ if (!localStorage.getItem("usuario")) {
     if (divBienvenida) {
         const mensajeBienvenida = document.createElement("h5");
         mensajeBienvenida.classList.add("text-center");
-        mensajeBienvenida.innerHTML = "Bienvenid@, " + usuario.nombre + " " + usuario.apellidoP;
+        mensajeBienvenida.innerHTML = "Bienvenid@, " + usuario.nombre + " " + usuario.apellidoP + " tu número de cuenta es: " + usuario.cuenta;
         divBienvenida.appendChild(mensajeBienvenida);
         let efectivoActual = usuario.efectivo ? usuario.efectivo : 0;
         document.getElementById("efectivo").innerHTML = parseFloat(efectivoActual).toLocaleString('en-US', {style: 'currency',currency: 'USD', minimumFractionDigits: 2});
@@ -63,7 +67,9 @@ const depositar = (monto) => {
                 usuario.efectivo = usuario.efectivo ? usuario.efectivo : 0;
                 usuario.efectivo = (parseFloat(usuario.efectivo) + parseFloat(monto)).toFixed(2);
                 usuarios[x] = usuario;
-                if (grabarMovimiento(monto, 1)) {
+                let respuesta = grabarMovimiento(monto, 1, usuario);
+                if (respuesta.res) {
+                    usuario = respuesta.user;
                     localStorage.setItem("usuarios", JSON.stringify(usuarios));
                     localStorage.setItem("usuario", JSON.stringify(usuario));
                     window.location.href = "./home.html";
@@ -86,6 +92,33 @@ if (formDepositar) {
             return;
         }
         depositar(monto);
+    })
+}
+
+const formTranferir = document.getElementById("formTranferir");
+if (formTranferir) {
+    formTranferir.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let monto = document.getElementById("montoDeposito").value;
+        let cuentaDestino = document.getElementById("cuentaDestino").value;
+        if (monto.length = 0 || isNaN(monto) || monto <= 0) {
+            document.getElementById("montoDeposito").value = "";
+            return;
+        }
+        if (cuentaDestino.length == 0) {
+            Swal.fire({
+                title: 'Ingresa la cuenta destino!',
+                text: '',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            }).then(function(isConfirm) {
+                // setTimeout(() => {
+                //     document.getElementById("cuentaDestino").focus();
+                // }, 500);
+            })
+            return;
+        }
+        tranferir(monto, cuentaDestino);
     })
 }
 
@@ -122,7 +155,9 @@ const retirar = (monto) => {
                 usuario.efectivo = usuario.efectivo ? usuario.efectivo : 0;
                 usuario.efectivo = (parseFloat(usuario.efectivo) - parseFloat(monto)).toFixed(2);
                 usuarios[x] = usuario;
-                if (grabarMovimiento(monto, 0)) {
+                let respuesta = grabarMovimiento(monto, 0, usuario);
+                if (respuesta.res) {
+                    usuario = respuesta.user;
                     localStorage.setItem("usuarios", JSON.stringify(usuarios));
                     localStorage.setItem("usuario", JSON.stringify(usuario));
                     window.location.href = "./home.html";
@@ -134,21 +169,86 @@ const retirar = (monto) => {
     }
 }
 
-const grabarMovimiento = (monto, tipoMovimiento) => {
+const tranferir = (monto, cuentaDestino) => {
     if (usuario) {
+
+        if (usuario.efectivo < parseFloat(monto)) {
+            Swal.fire({
+                title: 'Saldo Insuficiente!',
+                text: '',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            })
+            return;
+        }
+
+        let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+        let usuarioDestino = usuarios.filter(function (el) {
+            return el.cuenta == cuentaDestino;
+        });
+        if (!usuarioDestino || usuarioDestino.length == 0) {
+            Swal.fire({
+                title: 'La cuenta destino no existe!',
+                text: '',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            })
+            return;
+        }
+
+        usuarioDestino = usuarioDestino[0];
+
+        for (let x = 0; x < usuarios.length; x++) {
+            
+            if (usuario.cuenta == usuarios[x].cuenta) {
+                usuario.efectivo = usuario.efectivo ? usuario.efectivo : 0;
+                usuario.efectivo = (parseFloat(usuario.efectivo) - parseFloat(monto)).toFixed(2);
+                usuarios[x] = usuario;
+                let respuesta = grabarMovimiento(monto, 2, usuario);
+                if (respuesta.res) {
+                    usuario = respuesta.user;
+                    localStorage.setItem("usuario", JSON.stringify(usuario));
+                    for (let y = 0; y < usuarios.length; y++) {
+            
+                        if (usuarioDestino.cuenta == usuarios[y].cuenta) {
+                            usuarioDestino.efectivo = usuarioDestino.efectivo ? usuarioDestino.efectivo : 0;
+                            usuarioDestino.efectivo = (parseFloat(usuarioDestino.efectivo) + parseFloat(monto)).toFixed(2);
+                            let respuesta2 = grabarMovimiento(monto, 3, usuarioDestino);
+                            if (respuesta2.res) {
+                                usuarioDestino = respuesta2.user;
+                                usuarios[y] = usuarioDestino;                                
+                                localStorage.setItem("usuarios", JSON.stringify(usuarios));
+                                window.location.href = "./home.html";
+                            }
+                            break;
+                        }
+                        
+                    }
+                }
+                break;
+            }
+            
+        }
+        
+    }
+}
+
+const grabarMovimiento = (monto, tipoMovimiento, usuarioMovimiento) => {
+    if (usuarioMovimiento) {
         let total = parseFloat(monto).toFixed(2);
-        let fecha = new Date();
+        // let fecha = new Date();
+        var fecha = new Date().toISOString().slice(0, 10);
         let movimiento = {
             cantidad: total,
-            fecha: String(fecha.getDay() + '/' + fecha.getMonth() + '/' +  fecha.getFullYear()),
+            fecha: fecha,
             tipo: tipoMovimiento
         }
-        if (usuario.movimientos) {
-            usuario.movimientos.push(movimiento);
+        if (usuarioMovimiento.movimientos) {
+            usuarioMovimiento.movimientos.push(movimiento);
         } else {
-            usuario.movimientos = [movimiento];
+            usuarioMovimiento.movimientos = [movimiento];
         }
-        return true;
+        return {res: true, user: usuarioMovimiento};
     }
 }
 
